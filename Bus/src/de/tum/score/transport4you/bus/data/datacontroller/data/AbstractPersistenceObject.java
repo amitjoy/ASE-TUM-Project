2b10b222a529fe88c01632397409e51cda7f67e7
Amit Kumar Mondal <admin@amitinside.com>
2016-01-08 14:49:22 +0100

    Refactored JPA Persistence XML

---
+++ Bus/src/de/tum/score/transport4you/bus/data/datacontroller/data/AbstractPersistenceObject.java
@@ -0,0 +1,8 @@
+package de.tum.score.transport4you.bus.data.datacontroller.data;
+
+import java.io.Serializable;
+
+public abstract class AbstractPersistenceObject implements IAbstractPersistenceObject, Serializable{
+	
+
+}
---
+++ Bus/src/de/tum/score/transport4you/bus/data/datacontroller/data/IAbstractPersistenceObject.java
@@ -0,0 +1,10 @@
+package de.tum.score.transport4you.bus.data.datacontroller.data;
+
+public interface IAbstractPersistenceObject {
+	
+	/**
+	 * Returns the id
+	 */
+	public long getPersistenceId();
+
+}
