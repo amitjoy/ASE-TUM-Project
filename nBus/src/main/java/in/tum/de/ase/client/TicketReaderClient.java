cca8152ff9a78c7b021f5e3222620648f0a1ad96
Amit Kumar Mondal <admin@amitinside.com>
2016-01-12 02:58:49 +0100

    Refactoring

--- nBus/src/main/java/in/tum/de/ase/client/TicketReaderClient.java
+++ nBus/src/main/java/in/tum/de/ase/client/TicketReaderClient.java
@@ -2,13 +2,15 @@ package in.tum.de.ase.client;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.Window;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
@@ -25,13 +27,20 @@ public final class TicketReaderClient extends JFrame implements Runnable, Thread
 
 	private static final long serialVersionUID = 6441489157408381878L;
 
+	public static void centreWindow(final Window frame) {
+		final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		final int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+		final int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+		frame.setLocation(x, y);
+	}
+
 	public static void main(final String[] args) {
 		new TicketReaderClient();
 	}
 
-	private final Executor executor = Executors.newSingleThreadExecutor(this);
+	private final Executor executor = Executors.newCachedThreadPool();
+
 	private WebcamPanel panel = null;
-	private JTextArea textarea = null;
 
 	private Webcam webcam = null;
 
@@ -39,7 +48,7 @@ public final class TicketReaderClient extends JFrame implements Runnable, Thread
 		super();
 
 		this.setLayout(new FlowLayout());
-		this.setTitle("Scan Ticket");
+		this.setTitle("==== Scan Ticket =====");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		final Dimension size = WebcamResolution.QVGA.getSize();
@@ -50,29 +59,24 @@ public final class TicketReaderClient extends JFrame implements Runnable, Thread
 		this.panel = new WebcamPanel(this.webcam);
 		this.panel.setPreferredSize(size);
 
-		this.textarea = new JTextArea();
-		this.textarea.setEditable(false);
-		this.textarea.setPreferredSize(size);
-
 		this.add(this.panel);
-		this.add(this.textarea);
 
 		this.pack();
 		this.setVisible(true);
+		centreWindow(this);
 
 		this.executor.execute(this);
 	}
 
 	@Override
 	public Thread newThread(final Runnable r) {
-		final Thread t = new Thread(r, "example-runner");
+		final Thread t = new Thread(r, "ticket-scanner-worker");
 		t.setDaemon(true);
 		return t;
 	}
 
 	@Override
 	public void run() {
-
 		do {
 			try {
 				Thread.sleep(100);
@@ -100,7 +104,7 @@ public final class TicketReaderClient extends JFrame implements Runnable, Thread
 			}
 
 			if (result != null) {
-				this.textarea.setText(result.getText());
+				JOptionPane.showMessageDialog(null, result.getText(), "Ticket Validation", JOptionPane.PLAIN_MESSAGE);
 			}
 
 		} while (true);
