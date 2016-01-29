b5a3a7e223ee11d3b436f395cbe64a185564e751
Amit Kumar Mondal <admin@amitinside.com>
2016-01-29 12:20:07 +0100

    Added Ticket Resource Definition by Jan

--- Web/src/main/java/de/tum/score/transport4you/web/TicketResource.java
+++ Web/src/main/java/de/tum/score/transport4you/web/TicketResource.java
@@ -3,11 +3,23 @@ package de.tum.score.transport4you.web;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
-public class TicketResource extends ServerResource {
+import com.googlecode.objectify.ObjectifyService;
+
+public final class TicketResource extends ServerResource {
 
 	@Get
 	public String represent() throws Exception {
-		return null;
+		final Ticket ticket = ObjectifyService.ofy().load().type(Ticket.class)
+				.filter("ticketId", this.getAttribute("ticketId")).first().now();
+
+		if (ticket == null) {
+			System.err.println("Ticket with id '" + this.getAttribute("ticketId") + "' is null!");
+			return "";
+		}
+
+		String result = "";
+		result += ticket.getTicketId();
+		return result;
 	}
 
 }
