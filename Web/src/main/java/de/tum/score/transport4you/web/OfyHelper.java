bb1afaf982da994b9351b3c7f52588611e9d0e62
Amit Kumar Mondal <admin@amitinside.com>
2016-01-29 12:57:47 +0100

    Registered Ticket POJO

--- Web/src/main/java/de/tum/score/transport4you/web/OfyHelper.java
+++ Web/src/main/java/de/tum/score/transport4you/web/OfyHelper.java
@@ -1,25 +1,27 @@
 package de.tum.score.transport4you.web;
 
-import com.googlecode.objectify.Objectify;
-import com.googlecode.objectify.ObjectifyFactory;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 import com.googlecode.objectify.ObjectifyService;
 
-import javax.servlet.ServletContextListener;
-import javax.servlet.ServletContextEvent;
-
 /**
-* OfyHelper, a ServletContextListener, is setup in web.xml to run before a JSP is run.  This is
-* required to let JSP's access Ofy.
-**/
+ * OfyHelper, a ServletContextListener, is setup in web.xml to run before a JSP
+ * is run. This is required to let JSP's access Ofy.
+ **/
 public class OfyHelper implements ServletContextListener {
-	public void contextInitialized(ServletContextEvent event) {
-		// This will be invoked as part of a warmup request, or the first user request if no warmup
+	@Override
+	public void contextDestroyed(final ServletContextEvent event) {
+		// App Engine does not currently invoke this method.
+	}
+
+	@Override
+	public void contextInitialized(final ServletContextEvent event) {
+		// This will be invoked as part of a warmup request, or the first user
+		// request if no warmup
 		// request.
 		ObjectifyService.register(Group.class);
 		ObjectifyService.register(User.class);
-	}
-
-	public void contextDestroyed(ServletContextEvent event) {
-		// App Engine does not currently invoke this method.
+		ObjectifyService.register(Ticket.class);
 	}
 }
