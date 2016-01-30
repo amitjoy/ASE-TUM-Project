a654d63348dfe1ec4b3e40ad5fc9a997c7f93acb
Amit Kumar Mondal <admin@amitinside.com>
2016-01-30 12:16:28 +0100

    Modified Resource Delete Service

--- Web/src/main/java/de/tum/score/transport4you/web/TicketDeleteResource.java
+++ Web/src/main/java/de/tum/score/transport4you/web/TicketDeleteResource.java
@@ -1,17 +1,21 @@
 package de.tum.score.transport4you.web;
 
+import java.util.List;
+
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
 
 public final class TicketDeleteResource extends ServerResource {
 
 	@Get
 	public String represent() throws Exception {
-		ObjectifyService.ofy().delete().type(Ticket.class);
+		final List<Key<Ticket>> keys = ObjectifyService.ofy().load().type(Ticket.class).keys().list();
+		ObjectifyService.ofy().delete().keys(keys).now();
 
-		return "<ticket> ALL TICKETS DEACTIVATED. </ticket>";
+		return "<ticket> ALL TICKETS DEACTIVATED </ticket>";
 	}
 
 }
